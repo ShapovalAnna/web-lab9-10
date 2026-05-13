25cba89f435a6269b14ba915d5fa408816662e15
anna bc2025-3 <Anna.Shapoval@lnu.edu.ua>
2026-05-13 17:01:07 +0300

    Add mock train data

---
+++ main.structure/data/trains.js
@@ -0,0 +1,42 @@
+export const trains = [
+    {
+        id: 1,
+        number: "091К",
+        from: "Львів",
+        to: "Київ",
+        departure: "08:30",
+        duration: "5 год 20 хв"
+    },
+    {
+        id: 2,
+        number: "715Л",
+        from: "Львів",
+        to: "Одеса",
+        departure: "12:10",
+        duration: "10 год 15 хв"
+    },
+    {
+        id: 3,
+        number: "041П",
+        from: "Київ",
+        to: "Дніпро",
+        departure: "18:45",
+        duration: "7 год"
+    },
+    {
+        id: 4,
+        number: "029К",
+        from: "Київ",
+        to: "Ужгород",
+        departure: "21:00",
+        duration: "12 год 30 хв"
+    },
+    {
+        id: 5,
+        number: "145О",
+        from: "Івано-Франківськ",
+        to: "Харків",
+        departure: "16:20",
+        duration: "14 год 10 хв"
+    }
+];
