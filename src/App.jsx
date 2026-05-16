c9775bc124793ba9eaf1682abf434572833f8dbc
anna bc2025-3 <Anna.Shapoval@lnu.edu.ua>
2026-05-16 14:41:25 +0300

    Added navigation to booking page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Booking from "./pages/Booking";
+
 import "./App.css";
 
 export default function App() {
-    return <Home />;
+
+    return (
+        <Routes>
+
+            <Route
+                path="/"
+                element={<Home />}
+            />
+
+            <Route
+                path="/booking/:id"
+                element={<Booking />}
+            />
+
+        </Routes>
+    );
 }
